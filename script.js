8d9382c49f367bdcc3b84ea88bfdeb88605d229e
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2022-12-29 15:41:34 +0200

    updated

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ let inputval = document.querySelector("#cityinput");
 let btn = document.querySelector("#add");
 let city = document.querySelector("#cityoutput");
 let description = document.querySelector("#description");
+let feels = document.querySelector("#feelings");
 let temp = document.querySelector("#temp");
+let max = document.querySelector("#temp_max");
+let min = document.querySelector("#temp_min");
 let wind = document.querySelector("#wind");
+let press = document.querySelector("#pressure");
 
 let apik = "ec44ce25559821fab1c0f3ad2a937ea4";
 
@@ -21,13 +25,21 @@ btn.addEventListener('click', function(){
 
         let nameval = data['name']
         let descript = data['weather'][0]['description']
+        let feels_like = data['main']['feels_like']
         let temperature = data['main']['temp']
+        let tem_max = data['main']['temp_max']
+        let tem_min = data['main']['temp_min']
         let windspeed = data['wind']['speed']
+        let pressure = data['main']['pressure']
 
     city.innerHTML=`<span>${nameval}</span> weather forecast`
+    feels.innerHTML=`Feels like: <span>${convertion(feels_like)}C</span>`
     temp.innerHTML=`Temperature: <span>${convertion(temperature)}C</span>`
+    max.innerHTML=`Max temperature: <span>${convertion(tem_max)}C</span>`
+    min.innerHTML=`Min temperature: <span>${convertion(tem_min)}C</span>`
     description.innerHTML=`Sky Conditions: <span>${descript}</span>`
-    wind.innerHTML=`Wind Speed: <span>${windspeed}C</span>`
+    wind.innerHTML=`Wind Speed: <span>${windspeed}m/s</span>`
+    press.innerHTML=`Preassure: <span>${pressure}p</span>`
     })
 
     .catch(err => alert('Please, enter the locality name or correct name'));
